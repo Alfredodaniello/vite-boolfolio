9547532f3a227cdd2b90945da183f00002fa355a
Alfredodaniello <alfredo_daniello@libero.it>
2024-06-17 18:12:16 +0200

    feat: add call from axios and stamp data

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script>
+import ProjectCard from './components/ProjectCard.vue';
+export default {
+  components: {
+    ProjectCard
+  }
+}
+</script>
+
+<template>
+ <ProjectCard></ProjectCard>
+</template>
+
+<style lang="scss">
+@use './style/generic';
+</style>
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,44 @@
+<script>
+import axios from 'axios';
+export default {
+    name: 'ProjectCard',
+    data() {
+        return {
+            projects: []
+        };
+    },
+    methods: {
+        getProjects() {
+            axios.get('http://127.0.0.1:8000/api/projects')
+                .then((response) => {
+                this.projects = response.data.results
+            })
+        }
+    },
+    mounted() {
+        this.getProjects();
+    }
+}
+</script>
+
+<template>
+<h1 class="text-center">I miei progetti</h1>
+<div class="container">
+    <div class="row">
+        <div v-for="project in projects" class="col-4">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">{{project.title}}</h5>
+                    <small>{{project.slug}}</small>
+                    <p class="card-text">{{project.summary}}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</template>
+
+<style  scoped lang="scss">
+
+
+</style>
